d6ba6f2c36ee43faef576d331de7eb3e235f4056
karolh95 <karolh95@live.com>
2020-08-25 10:37:40 +0200

    Added Material Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './modules/material.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		MaterialModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
